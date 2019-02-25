085f6a143071b5e0f60e3a2141b2f9067f3f49c8
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2019-02-25 16:46:46 +0000

    Move things into a subdirectory, so Github shows the README clearly

--- main.mk
+++ main.mk
@@ -1,9 +1,9 @@
 BUILD_DIR := intermediate
 TARGET_DIR := ./
 
-SUBMAKEFILES := common.mk glasgow_subgraph_solver.mk glasgow_clique_solver.mk
+SUBMAKEFILES := src/common.mk src/glasgow_subgraph_solver.mk src/glasgow_clique_solver.mk
 
 boost_ldlibs := -lboost_thread -lboost_system -lboost_program_options
 
-override CXXFLAGS += -O3 -march=native -std=c++17 -I./ -W -Wall -g -ggdb3 -pthread
+override CXXFLAGS += -O3 -march=native -std=c++17 -Isrc/ -W -Wall -g -ggdb3 -pthread
 override LDFLAGS += -pthread
