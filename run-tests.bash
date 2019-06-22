2b35101ab34e9fc641d0692fc2f566e4ffcd3ee1
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2019-06-22 22:59:00 +0100

    Brace expansion inside ducks breaks older bash

--- run-tests.bash
+++ run-tests.bash
@@ -1,26 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash
 
-if ! grep '^status = true$' <(./glasgow_subgraph_solver --format lad test-instances/{small,large} ) ; then
+set -x
+
+if ! grep '^status = true$' <(./glasgow_subgraph_solver --format lad test-instances/small test-instances/large ) ; then
     echo "non-induced test failed" 1>&1
     exit 1
 fi
 
-if ! grep '^status = false$' <(./glasgow_subgraph_solver --induced --format lad test-instances/{small,large} ) ; then
+if ! grep '^status = false$' <(./glasgow_subgraph_solver --induced --format lad test-instances/small test-instances/large ) ; then
     echo "induced test failed" 1>&1
     exit 1
 fi
 
-if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions --format lad test-instances/{small,large} ) ; then
+if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions --format lad test-instances/small test-instances/large ) ; then
     echo "non-induced enumerate test failed" 1>&1
     exit 1
 fi
 
-if ! grep '^solution_count = 12$' <(./glasgow_subgraph_solver --count-solutions test-instances/{trident,longtrident}.csv ) ; then
+if ! grep '^solution_count = 12$' <(./glasgow_subgraph_solver --count-solutions test-instances/trident.csv test-instances/longtrident.csv ) ; then
     echo "trident enumerate test failed" 1>&1
     exit 1
 fi
 
-if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions --induced test-instances/{c3,c3c2}.csv ) ; then
+if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions --induced test-instances/c3.csv test-instances/c3c2.csv ) ; then
     echo "induced cyclic enumerate test failed" 1>&1
     exit 1
 fi
