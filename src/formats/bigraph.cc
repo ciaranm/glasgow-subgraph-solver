63b34395e4d13c9e825f356bcdc51bad7eb7d17a
Ciaran McCreesh <ciaran.mccreesh@glasgow.ac.uk>
2020-08-07 18:42:13 +0100

    Start reformatting a bit

--- src/formats/bigraph.cc
+++ src/formats/bigraph.cc
@@ -39,32 +39,34 @@ namespace
 auto read_target_bigraph(ifstream && infile, const string & filename) -> InputGraph
 {
     InputGraph result{ 0, true, true, true };
-    
-    int r = read_num(infile); 
-    int n = read_num(infile);
-    int s = read_num(infile); 
-    int h = read_num(infile);
-    result.resize(r+n+s);
 
-    for (int i=0; i<r; i++) {
+    int r = read_num(infile);
+    int n = read_num(infile);
+    int s = read_num(infile);
+    int h = read_num(infile);
+    result.resize(r + n + s);
+
+    for (int i = 0 ; i != r ; ++i) {
         result.set_vertex_label(i, "ROOT");
         result.set_vertex_name(i, "ROOT" + to_string(i));
     }
 
-    for (int i=(r+n); i<(r+n+s); i++) {
+    for (int i = (r + n) ; i < (r + n + s) ; ++i) {
         result.set_vertex_label(i, "SITE");
         result.set_vertex_name(i, "SITE" + to_string(i));
     }
 
-    for (int i=r; i<(r+n); i++) {
+    for (int i = r ; i < (r + n) ; ++i) {
         result.set_vertex_label(i, read_str(infile));
-        result.set_vertex_name(i, to_string(i-r));
+        result.set_vertex_name(i, to_string(i - r));
     }
- 
-    for (int i=0; i<(r+n); i++) for(int j=r; j<(n+s+r); j++) {
-        char x = read_char(infile);
-        if (x == '1') result.add_directed_edge(i, j, "dir");
-    }
+
+    for (int i = 0 ; i != (r + n) ; ++i)
+        for (int j = r ; j != (n + s + r) ; ++j) {
+            char x = read_char(infile);
+            if (x == '1')
+                result.add_directed_edge(i, j, "dir");
+        }
 
     for (int i=0; i<h; i++) {
         std::pair<bool, std::vector<int> > he;
@@ -75,7 +77,7 @@ auto read_target_bigraph(ifstream && infile, const string & filename) -> InputGr
             int index = std::stoi(x);
             he.second[index-1+r]++;
             x = read_str(infile);
-        }   
+        }
         if(x == "t") he.first = true;
         else he.first = false;
 
@@ -88,15 +90,15 @@ auto read_target_bigraph(ifstream && infile, const string & filename) -> InputGr
 auto read_pattern_bigraph(ifstream && infile, const string & filename) -> InputGraph
 {
     InputGraph result{ 0, true, true, true };
-    
-    int r = read_num(infile); 
+
+    int r = read_num(infile);
     int n = read_num(infile);
-    int s = read_num(infile); 
+    int s = read_num(infile);
     int h = read_num(infile);
     result.resize(n);
 
     for (int i=0; i<n; i++) result.set_vertex_label(i, read_str(infile));
-  
+
     for (int i=0; i<(r+n); i++) for(int j=0; j<(n+s); j++) {
         char x = read_char(infile);
         if (i >= r && j < n && x == '1') result.add_directed_edge(i-r, j, "dir");
@@ -119,10 +121,10 @@ auto read_pattern_bigraph(ifstream && infile, const string & filename) -> InputG
             int index = std::stoi(x);
             he.second[index-1]++;
             x = read_str(infile);
-        }   
+        }
         if(x == "t") he.first = true;
         else he.first = false;
-        
+
         result.add_hyperedge(he);
     }
 
