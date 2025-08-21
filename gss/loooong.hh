331bc039f0e6dd5481552ca83fb5230442453143
Mark Turnbull <mrktrnbll@gmail.com>
2025-08-21 12:18:28 +0100

    [WIP] - removing the use of multiprecision::cpp_int, using maybe more annoying gmp but faster for larger int...

--- gss/loooong.hh
+++ gss/loooong.hh
@@ -1,11 +1,11 @@
 #ifndef GLASGOW_SUBGRAPH_SOLVER_GUARD_SRC_LOOOONG_HH
 #define GLASGOW_SUBGRAPH_SOLVER_GUARD_SRC_LOOOONG_HH 1
 
-#include <boost/multiprecision/cpp_int.hpp>
+#include <gmpxx.h>
 
 namespace gss
 {
-    using loooong = boost::multiprecision::cpp_int;
+    using loooong = mpz_class;
 }
 
 #endif
